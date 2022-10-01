625e4533d3d3608d9c2e08452f14d7ec359a3368
Oscar Acevedo <oacevedovilches@gmail.com>
2022-10-01 17:34:02 -0400

    added js and context

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppProvider from "./MyContext";
+import Favorites from "./views/Favorites";
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="*" element={<h1>:( Pagina no encontrada</h1>} />
+        </Routes>
+      </AppProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
